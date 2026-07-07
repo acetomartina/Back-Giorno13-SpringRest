9456e8228a1d1c1052dd131432c238a1d7539009
Martina <acetomartina141@gmail.com>
2026-07-07 15:57:49 +0200

    Initial commit

---
+++ src/main/java/com/martina/Back_Giorno13_SpringRest/BackGiorno13SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.martina.Back_Giorno13_SpringRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGiorno13SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGiorno13SpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martina/Back_Giorno13_SpringRest/BackGiorno13SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martina.Back_Giorno13_SpringRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackGiorno13SpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
